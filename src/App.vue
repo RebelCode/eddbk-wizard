5b9a3f1b526bc1cb7eca404ffade70fefca7de2b
Refat <refat.ametov@gmail.com>
2017-08-14 14:54:33 +0300

    implement 4-step wizard skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <BookingWizard/>
   </div>
 </template>
 
 <script>
+import BookingWizard from '@/components/BookingWizard.vue'
+
 export default {
   name: 'app',
   components: {
+    BookingWizard
   }
 }
 </script>
---
+++ src/components/BookingWizard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form-wizard title="Book an appointment" subtitle="">
+      <tab-content title="Service">
+        Service tab content
+      </tab-content>
+      <tab-content title="Date &amp; time">
+          Date &amp; time tab content
+       </tab-content>
+       <tab-content title="Payment">
+         Payment tab content
+       </tab-content>
+       <tab-content title="Confirmation">
+         Confirmation tab content
+       </tab-content>
+    </form-wizard>
+  </div>
+</template>
+
+<script>
+import {FormWizard, TabContent} from 'vue-form-wizard'
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+
+export default {
+  components: {
+    FormWizard,
+    TabContent
+  }
+}
+
+</script>
