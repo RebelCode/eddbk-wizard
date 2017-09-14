6003ba9e8fac1e7dde3f1436c281bdfe0cfbd3b8
Refat <refat.ametov@gmail.com>
2017-09-14 17:11:43 +0300

    replace the dummy string concationation with array.join()

--- src/store/modules/calendar/mutations.js
+++ src/store/modules/calendar/mutations.js
@@ -7,12 +7,14 @@ export default {
   insertAsSessionsTree (state, { collection }) {
     _.each(collection, session => {
       const sessionStart = moment.unix(session.start)
-      const objectPath = session.serviceId + '.' +
-        sessionStart.year() + '.' +
-        sessionStart.month() + '.' +
-        sessionStart.date() + '.' +
-        session.duration + '.' +
+      const objectPath = [
+        session.serviceId,
+        sessionStart.year(),
+        sessionStart.month(),
+        sessionStart.date(),
+        session.duration,
         session.uId
+      ].join('.')
       vueSet(state.allSessions, objectPath, session)
     })
   }
