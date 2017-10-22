4608a1e423d00ddbe7e0b7124255eb990831d28c
Refat <refat.ametov@gmail.com>
2017-10-23 02:19:14 +0300

    refactoring, implement external DI container (inside the Bottle)

--- src/main.js
+++ src/main.js
@@ -1,29 +1,14 @@
-// @flow
+import bottle from '@/services/'
+import App from './app.js'
 
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import Vuex from 'vuex'
-import ServicesPlugin from './services/plugin.js'
-import store from './store'
-import App from './App.vue'
-
-Vue.use(ServicesPlugin)
-Vue.use(Vuex)
-Vue.config.productionTip = false
-
-const BookingWizard = (options: {
-  el: string,
-  translations: Object
-}) => {
-  const vueInstance = new Vue({
-    el: options.el,
-    template: '<App/>',
-    store,
-    components: { App }
-  })
-
-  return vueInstance
+const BookingWizardManager = {
+  bottle,
+  getDefaultBottle () {
+    return Object.assign(Object.create(bottle), bottle) // clone
+  },
+  create (bottle = this.getDefaultBottle()) {
+    return new App(bottle)
+  }
 }
 
-window.BookingWizard = BookingWizard
+window.BookingWizardManager = BookingWizardManager
--- src/services/plugin.js
+++ src/services/plugin.js
@@ -1,8 +1,6 @@
-import di from '@/services'
-
 const ServicesPlugin = {
-  install (Vue, options) {
-    const services = di.container
+  install (Vue, bottle) {
+    const services = bottle.container
     Vue.$services = Vue.prototype.$services = services
 
     // shorthands for common services
@@ -10,8 +8,8 @@ const ServicesPlugin = {
     Vue.$_ = Vue.prototype.$_ = services.translate
     Vue.$sm = Vue.prototype.$sm = services.store
     Vue.$api = Vue.prototype.$api = services.api
-    Vue.$repo = Vue.prototype.$repo = services.repo
     Vue.$eventBus = Vue.prototype.$eventBus = services.eventBus
+    Vue.$config = Vue.prototype.$config = services.config
   }
 }
 
