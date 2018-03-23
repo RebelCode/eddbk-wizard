25066047a36c708b061b727e4023be6e1dc291fd
Roman Lytvynenko <litvinenko95@gmail.com>
2018-03-23 19:02:29 +0200

    doc for new mapStore

--- src/utils/vuex/index.js
+++ src/utils/vuex/index.js
@@ -1,5 +1,44 @@
 import _ from 'lodash'
 
+/**
+ * Helper function for mapping Vuex state and getters with API similar
+ * to Vuex's map*Something* helpers.
+ *
+ * It will create computed properties with inner `get()` and `set(value)` methods
+ * for items in state and for getters. For getters there is only `get()` available,
+ * but for items in state `set(value)` is also available which allows us to set state item`s
+ * value in code using `=`:
+ * ```js
+ * this.fooInStore = 'bar'
+ * ```
+ *
+ * For example you have store module in namespace `basket` with two items
+ * in state: `selectedApple`, `list` and one getter `greenApplesList`.
+ *
+ * Using this helper map function you can simply write:
+ * ```js
+ * computed: {
+ *   ...mapStore('basket', [
+ *     'selectedApple',
+ *     'list',
+ *     'greenApplesList'
+ *   ])
+ * }
+ * ```
+ * and then in your code you can write:
+ * ```js
+ * this.selectedApple = this.greenApplesList[1]
+ * ```
+ *
+ * Under the hood it will execute `set` mutation on bucket's store
+ * ans set greenApplesList[1] as `selectedApple's` value.
+ *
+ * @param namespace {string} Store module's namespace name
+ * @param map {array|object} State or getter keys in store to map.
+ *                           Use object if you want to use different name in consuming code:
+ *                           `{'preferredName': 'realNameInStore'}`
+ * @return {*}
+ */
 const mapStore = (namespace, map) => {
   if (Array.isArray(map)) {
     const _map = {}
